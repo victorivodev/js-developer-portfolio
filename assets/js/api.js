07bbf366cf01fa5da3f659325eaa6eb73058e5b0
victorivodev <victorivo.comercial@gmail.com>
2023-12-12 15:06:51 -0300

    Commit final

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
-async function fetchProfile(){
-    const url = "linkrawgithub"
+async function fetchProfileData(){
+    const url = "https://raw.githubusercontent.com/victorivodev/js-developer-portfolio/main/data/profile.json"
     fetch(url)
     const fetching = await fetch(url)
     return fetching.json()
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,74 @@
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+
+    const phone = document.getElementById('profile.phone')
+    phone.innerText = profileData.phone
+    phone.href = `:tel${profileData.phone}`
+
+    const email = document.getElementById('profile.email')
+    email.innerText = profileData.email
+    email.href = `:mailto${profileData.email}`
+}
+
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softSkills')
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
+function updateHardSkills(profileData){
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
+}
+
+function updateLanguages(profileData) {
+    const languages = document.getElementById('profile.languages')
+    languages.innerHTML = profileData.languages.map(languages => `<li>${languages}</li>`).join('')
+}
+
+function updatePortfolio(profileData){
+    const portfolio = document.getElementById('profile.portfolio')
+    portfolio.innerHTML = profileData.portfolio.map(project => {
+        return `
+        <li>
+            <span ${project.github ? 'class="titulo github"' : ''}>${project.name}</span>
+            <a href="${project.url}" target="_blank">${project.url}</a>
+        </li>
+        `
+    }).join('')
+}
+
+function updateProfessionalExperience (profileData) {
+    const professionalExperience = document.getElementById('profile.professionalExperience')
+    professionalExperience.innerHTML = profileData.professionalExperience.map(professionalExperience => {
+        return `
+        <li>
+        <span class="titulo" id="profile.professionalExperience.name">${professionalExperience.name}</span>
+        <span class="periodo" id="profile.professionalExperience.period">${professionalExperience.period}</span>
+        <p id="profile.professionalExperience.description">
+          ${professionalExperience.description}
+        </p>
+      </li>
+      `
+    }).join('')
+}
+
 (async () => {
     const profileData = await fetchProfileData()
-    console.log(fetchProfile)
+    updateProfileInfo(profileData)
+    updateSoftSkills(profileData)
+    updateHardSkills(profileData)
+    updateLanguages(profileData)
+    updatePortfolio(profileData)
+    updateProfessionalExperience(profileData)
 })()
